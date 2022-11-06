f2a9b588346d2a6135801323ec392e806cf550ad
YRMING <yrm@outlook.com>
2022-11-06 20:11:54 +0800

    feat: show fn name and description

--- webview/src/views/doc/Doc.vue
+++ webview/src/views/doc/Doc.vue
@@ -3,13 +3,13 @@ import type { VueUseFunction } from '@vueuse/metadata'
 import { ref } from 'vue'
 import FunctionInfo from './components/FunctionInfo.vue'
 
-const fn = ref('')
+const fn = ref<VueUseFunction | null>(null)
 
 window.addEventListener('message', (event) => {
   const message = event.data
   switch (message.command) {
     case 'showDoc':
-      fn.value = (message.fn as VueUseFunction).name
+      fn.value = message.fn
       break
   }
 })
@@ -17,12 +17,19 @@ window.addEventListener('message', (event) => {
 
 <template>
   <div class="doc" v-if="fn">
-    <FunctionInfo :fn="fn" />
+    <h1 class="op-85">{{ fn.name }}</h1>
+    <FunctionInfo :fn="fn.name" />
+    <p class="text-base op-85">{{ fn.description }}</p>
   </div>
 </template>
 
 <style scoped>
 .doc {
-
+  padding: 10px 32px;
+  max-width: 882px;
+  margin: 0 auto;
+}
+h2 {
+  @apply mt-10px mb-16px pt-24px;
 }
 </style>
