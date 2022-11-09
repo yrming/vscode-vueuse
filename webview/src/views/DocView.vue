17a06a55fbf9907a9ad3f53d78c9635b325a6514
YRMING <yrm@outlook.com>
2022-11-09 22:01:33 +0800

    fix: fn description

--- webview/src/views/DocView.vue
+++ webview/src/views/DocView.vue
@@ -5,6 +5,7 @@ import { functions } from '@vueuse/metadata'
 import type { VueUseFunction } from '@vueuse/metadata'
 import { provideVSCodeDesignSystem, vsCodeDivider } from '@vscode/webview-ui-toolkit'
 import FunctionInfo from '@/components/FunctionInfo.vue'
+import { renderMarkdown } from '@/utils/common'
 
 provideVSCodeDesignSystem().register(vsCodeDivider())
 
@@ -31,14 +32,14 @@ function goBack() {
   <div class="doc m-auto" v-if="fn">
     <div class="h-8 flex items-center justify-between">
       <span class="cursor-pointer w-8 h-8 flex items-center" @click="goBack">
-        <i class="i-carbon:arrow-left opacity-90 text-xs scale-140 inline-block" />
+        <i class="i-carbon:arrow-left op-85 text-xs scale-140 inline-block" />
       </span>
-      <span class="text-base op-90">{{ fn.name }}</span>
-      <span class="w-8 h-8"></span>
+      <span class="text-base op-90 mx-4">{{ fn.name }}</span>
+      <span class="op-85 cursor-pointer py-2">Return to top</span>
     </div>
     <vscode-divider></vscode-divider>
     <FunctionInfo :fn="fn.name" />
-    <p class="text-base op-85">{{ fn.description }}</p>
+    <p class="text-base op-85" v-html="renderMarkdown(fn.description)"></p>
   </div>
 </template>
 
