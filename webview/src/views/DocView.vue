812b706a4efa08c6622c5fd1ef5831a1e3f26b59
YRMING <yrm@outlook.com>
2022-11-09 21:44:47 +0800

    feat: add nav for doc view

--- webview/src/views/DocView.vue
+++ webview/src/views/DocView.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { functions } from '@vueuse/metadata'
 import type { VueUseFunction } from '@vueuse/metadata'
+import { provideVSCodeDesignSystem, vsCodeDivider } from '@vscode/webview-ui-toolkit'
 import FunctionInfo from '@/components/FunctionInfo.vue'
 
+provideVSCodeDesignSystem().register(vsCodeDivider())
+
 const route = useRoute()
+const router = useRouter()
 
 const fn = ref<VueUseFunction | null | undefined>(null)
 
@@ -15,28 +19,27 @@ onMounted(() => {
     fn.value = functions.find((i) => i.name === fnName)
   }
 })
+
+function goBack() {
+  router.replace({
+    name: 'home'
+  })
+}
 </script>
 
 <template>
-  <div class="doc" v-if="fn">
-    <div class="h-8 px-1">
-      <i class="i-carbon:arrow-left opacity-80 text-xs" />
+  <div class="doc m-auto" v-if="fn">
+    <div class="h-8 flex items-center justify-between">
+      <span class="cursor-pointer w-8 h-8 flex items-center" @click="goBack">
+        <i class="i-carbon:arrow-left opacity-90 text-xs scale-140 inline-block" />
+      </span>
+      <span class="text-base op-90">{{ fn.name }}</span>
+      <span class="w-8 h-8"></span>
     </div>
-    <i class="i-carbon-search mr-2 op-50 scale-120"></i>
-    <i class="i-carbon-launch opacity-80 text-xs ml-1" />
-    <h1 class="op-85">{{ fn.name }}</h1>
+    <vscode-divider></vscode-divider>
     <FunctionInfo :fn="fn.name" />
     <p class="text-base op-85">{{ fn.description }}</p>
   </div>
 </template>
 
-<style scoped>
-.doc {
-  /* padding: 10px 32px;
-  max-width: 882px; */
-  margin: 0 auto;
-}
-h2 {
-  @apply mt-10px mb-16px pt-24px;
-}
-</style>
+<style scoped></style>
